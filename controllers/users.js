4365d9467fa52c52deb1c0f2b8808e1ebf8d1a4b
Anastasia Dubkovetskaya <k1tanas@yandex.ru>
2022-06-06 23:45:48 +0200

    add cookie

--- controllers/users.js
+++ controllers/users.js
@@ -72,6 +72,7 @@ const login = (req, res, next) => {
         res.cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           sameSite: 'none',
+          secure: true,
           httpOnly: true,
         });
         res.send({ _id: user._id });
